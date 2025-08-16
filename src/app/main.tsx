1fa30e3544c4da687d441152fa01d4f9a66ba4ce
LUK JIA MING <jiaming@LUKs-Air.lan>
2025-08-17 00:38:39 +0800

    refactor menu items

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,14 +1,34 @@
 "use client"
-import { AppSideBar } from "@/components/ui/AppSideBar";
+import { AppSideBar, MenuGroup } from "@/components/ui/AppSideBar";
 import { SidebarProvider, SidebarTrigger, useSidebar } from "@/components/ui/sidebar";
 import { useEffect, useState } from "react";
+
+const menuGroups: MenuGroup[] = [
+    {
+        title: "Tools",
+        items: [
+            {
+                title: "TLV Decoder",
+                path: "/tlv-decoder"
+            }, 
+            {
+                title: "Json Formatter",
+                path: "/json-formatter"
+            }, 
+            {
+                title: "String Encoder",
+                path: "/byte-converter"
+            }
+        ]
+    }, 
+]
 
 function Main({ children }: { children: React.ReactNode }) {
     const [open, setOpen ] = useState(false)
  
     return ( 
     <SidebarProvider open={open} onOpenChange={setOpen}>
-        <AppSideBar />
+        <AppSideBar menuGroups={menuGroups}/>
         <main className="w-full h-[100vh]">
             <SidebarTrigger />
             {children}
--- src/components/ui/AppSideBar.tsx
+++ src/components/ui/AppSideBar.tsx
@@ -1,71 +1,70 @@
 
 import { Toaster } from "sonner";
-import {Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarGroupContent, SidebarMenu, SidebarMenuItem, SidebarTrigger, SidebarMenuButton, useSidebar } from "./sidebar";
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupLabel, SidebarGroupContent, SidebarMenu, SidebarMenuItem, SidebarTrigger, SidebarMenuButton, useSidebar } from "./sidebar";
 import Link from "next/link";
 import { AppRoute } from "next/dist/build/swc/types";
 import { AppRouteNames, Value } from "@/routes/routes";
 
 
-// Menu items.
-const items = [
-  {
-    title: "Home",
-    url: "#",
-  },
-  {
-    title: "Inbox",
-    url: "#",
-  },
-  {
-    title: "Calendar",
-    url: "#",
-  },
-  {
-    title: "Search",
-    url: "#",
-  },
-  {
-    title: "Settings",
-    url: "#",
-  },
-]
+export interface MenuGroup {
+    title: string,
+    items: MenuItem[]
+}
 
-export interface AppSideBarProps {
-    onMenuItemClicked: () => void
+interface MenuItem {
+    title: string,
+    path: string
+}
+
+interface AppSideBarProps {
+    menuGroups: MenuGroup[]
 }
 
 
-function AppSideBar() {
-    const { setOpenMobile } = useSidebar()
+function AppSideBar({ menuGroups }: AppSideBarProps) {
     return (
-    <Sidebar>
-      <SidebarContent>
-        <SidebarGroup >
-          <SidebarGroupLabel>Tools</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild onClick={() => setOpenMobile(false)}>
-                    <Link href="/tlv-decoder">TLV Decoder</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild onClick={() => setOpenMobile(false)}>
-                    <Link href="/json-formatter">JSON Formatter</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-             <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                    <Link href="/byte-converter">Byte Converter</Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-      </SidebarContent>
+        <Sidebar>
+            <SidebarContent>
+                {menuGroups.map((group) => (
+                    <MenuGroupView
+                        key={group.title}
+                        menuGroup={group}
+                    />
+                ))}
+            </SidebarContent>
 
-      <Toaster richColors position="top-center" />
-    </Sidebar>
+            <Toaster richColors position="top-center" />
+        </Sidebar>
+    )
+}
+
+function MenuGroupView({ menuGroup }: { menuGroup: MenuGroup }) {
+    return (
+        < SidebarGroup >
+            <SidebarGroupLabel>{ menuGroup.title }</SidebarGroupLabel>
+            <SidebarGroupContent>
+                {menuGroup.items.map((menuItem) => (
+                    <MenuItemView
+                        key={menuItem.path}
+                        menuItem={menuItem}
+                    />
+                ))}
+            </SidebarGroupContent>
+        </SidebarGroup >
+    )
+}
+
+function MenuItemView({ menuItem }: { menuItem: MenuItem }) {
+    const { setOpenMobile } = useSidebar()
+
+    return (
+        <SidebarMenu>
+            <SidebarMenuItem>
+                <SidebarMenuButton asChild onClick={() => setOpenMobile(false)}>
+                    <Link href={menuItem.path}>{menuItem.title}</Link>
+                </SidebarMenuButton>
+            </SidebarMenuItem>
+        </SidebarMenu>
     )
 }
 
