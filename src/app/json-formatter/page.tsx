22c198e0f55add5fbcc0420b236d1f78d3f142f6
LUK JIA MING <jiaming@LUKs-Air.lan>
2025-08-16 22:55:22 +0800

    json formatter layout fix

--- src/app/json-formatter/page.tsx
+++ src/app/json-formatter/page.tsx
@@ -23,7 +23,7 @@ export default function JsonFormatter() {
 
       const parsed = JSON.parse(input)
       const pretty = JSON.stringify(parsed, null, 2)
-      setOutput(pretty)
+      setInput(pretty)
       setError(null)
     } catch (err) {
       setError("Invalid JSON")
@@ -32,31 +32,17 @@ export default function JsonFormatter() {
   }
 
   return (
-    <div className="flex p-6 h-full w-full">
-      {/* Left: Input */}
-      <div className="flex flex-col m-6 flex-1">
-        <Textarea
+    <div className="flex flex-col p-6 h-full w-full items-center">
+      <Textarea
           value={input}
           onChange={(e) => setInput(e.target.value)}
           placeholder='Paste raw JSON here...'
-          className="flex-1 break-all rounded-lg border border-zinc-200 bg-white text-sm"
+          className="rounded-lg border break-all border-zinc-200 bg-white text-sm h-[60vh]"
         />
-        <Button onClick={formatJson} className="mt-3">Format</Button>
-        {error && <p className="text-red-500 mt-2">{error}</p>}
-      </div>
-
-
-      {/* Right: Output */}
-      <ScrollArea className="flex-1 rounded-lg border border-zinc-200 bg-white p-4 m-6 shadow-inner">
-        {error ? (
-          <Card className="bg-red-50 border-red-300 p-6">
-            <p className="text-red-600 font-mono text-sm">{error}</p>
-          </Card>) : (
-          <pre className="whitespace-pre-wrap text-sm text-zinc-500 break-all">
-            {output ? output : "Pretty output here"}
-          </pre>
-        )}
-      </ScrollArea>
+        <Button onClick={formatJson} className="mt-3 w-full items-center button">Format</Button>
+        <div className="pt-2 pb-2 w-full">
+          {error && <p className="text-red-500 text-sm">{error}</p>}
+        </div>
     </div>
   )
 }
